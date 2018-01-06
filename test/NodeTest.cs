f55aa5cc76ebac150560e497746a98145e281524
Jacob Eisenberg <jei@trifork.com>
2018-01-06 08:43:35 +0100

    Add test for edges

--- test/NodeTest.cs
+++ test/NodeTest.cs
@@ -11,6 +11,15 @@ namespace NavigationApi.Test
             var a = new Node("a");
             Assert.Equal("a", a.Id);
         }
+
+        [Fact]
+        public void Can_have_edges()
+        {
+            var a = new Node("a");
+            var b = new Node("b");
+            a.Edges.Add(new Edge(b));
+            Assert.Equal(1, a.Edges.Count);
+        }
     }
 
     public class Node
@@ -20,6 +29,18 @@ namespace NavigationApi.Test
             Id = id;
         }
 
-        public string Id { get; private set; }
+        public string Id { get; }
+
+        public IList<Edge> Edges { get; } = new List<Edge>();
+    }
+
+    public class Edge
+    {
+        public Edge(Node neighbour)
+        {
+            Neighbour = neighbour;
+        }
+
+        public Node Neighbour { get; }
     }
 }
