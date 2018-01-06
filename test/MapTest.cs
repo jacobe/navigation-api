3bdaeab8045b77852dd316e69784ea0ff317dccc
Jacob Eisenberg <jei@trifork.com>
2018-01-06 08:57:37 +0100

    Add test for a Map class having nodes

---
+++ test/MapTest.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace NavigationApi.Test
+{
+    public class MapTest
+    {
+        [Fact]
+        public void Has_a_required_id()
+        {
+            var m1 = new Map("m1");
+            Assert.Equal("m1", m1.Id);
+        }
+
+        [Fact]
+        public void Has_nodes()
+        {
+            var m1 = new Map("m1");
+            m1.Nodes.Add(new Node("a"));
+            Assert.Equal(1, m1.Nodes.Count);
+        }
+    }
+
+    public class Map
+    {
+        public Map(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; }
+        public IList<Node> Nodes { get; } = new List<Node>();
+    }
+}
